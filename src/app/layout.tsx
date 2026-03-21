c2de035e9af29dd9370cda177ad9b3bccf5ee52f
Killian Grant <killian@killiangrant.com>
2026-03-21 13:15:21 -0600

    vertical align body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={'flex items-center justify-center'}>{children}</body>
+    <html lang="en" className={'flex items-center justify-center min-h-full'}>
+      <body>{children}</body>
     </html>
   );
 }
