95035f3a1206c84d3dc78ff05c01cd0d30d690e8
Killian Grant <killian@killiangrant.com>
2026-04-21 10:29:24 -0600

    add text to links

--- src/components/What.tsx
+++ src/components/What.tsx
@@ -8,6 +8,17 @@ export default function What() {
         <li>Engineering Leadership</li>
         <li>Full-Stack Engineering</li>
         <li>UX/UI Design</li>
+        <li>
+          <a
+            href={
+              'https://github.com/nailliK/resumes/releases/download/2026-03-21/KillianLouisGrant-QualificationsSummary-GeneralistProfile.pdf'
+            }
+            title={'Résumé'}
+            download
+          >
+            📄 Download my Résumé
+          </a>
+        </li>
       </ul>
     </section>
   );
--- src/components/Where.tsx
+++ src/components/Where.tsx
@@ -4,41 +4,35 @@ export default function Where() {
   return (
     <section>
       <h3>Where</h3>
-      <ul className={'flex gap-4'}>
+      <ul>
         <li>
-          <a href={'sms:+17202319248'} title={'SMS'}>
-            📞
+          <a href={'tel:+17202319248'} title={'Phone'}>
+            📞 Phone
           </a>
         </li>
         <li>
           <a href={'mailto:killian@killiangrant.com'} title={'Email'}>
-            📧
+            📧 Email
           </a>
         </li>
         <li>
           <a href={'https://linkedin.com/in/killianlgrant'} target={'_blank'} title={'LinkedIn'}>
-            👔
+            👔 LinkedIn
           </a>
         </li>
         <li>
           <a href={'https://github.com/nailliK'} target={'_blank'} title={'Github'}>
-            🐙
+            🐙 Github
           </a>
         </li>
         <li>
           <a href={'https://secrethandshake.io'} target={'_blank'} title={'Secret Handshake'}>
-            🤝
+            🤝 Secret Handshake
           </a>
         </li>
         <li>
-          <a
-            href={
-              'https://github.com/nailliK/resumes/releases/download/2026-03-21/KillianLouisGrant-QualificationsSummary-GeneralistProfile.pdf'
-            }
-            title={'Résumé'}
-            download
-          >
-            📄
+          <a href={'https://iamslime.com'} target={'_blank'} title={'SLIME'}>
+            🫠 SLIME
           </a>
         </li>
       </ul>
